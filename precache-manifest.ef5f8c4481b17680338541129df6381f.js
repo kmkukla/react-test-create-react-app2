9d2e64ad7bb768fbc5e277546e2ce09edbc4a325
kmkukla <kondziocard@gmail.com>
2020-07-29 17:27:14 +0000

    Initial commit

---
+++ precache-manifest.ef5f8c4481b17680338541129df6381f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecee1213aa06aa81af957319a45f5bc",
+    "url": "/react-test-create-react-app/index.html"
+  },
+  {
+    "revision": "5f812d2030b77f1ac945",
+    "url": "/react-test-create-react-app/static/css/main.7a268217.chunk.css"
+  },
+  {
+    "revision": "96bbd11f784f4d55efb7",
+    "url": "/react-test-create-react-app/static/js/2.c5f573e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-create-react-app/static/js/2.c5f573e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f812d2030b77f1ac945",
+    "url": "/react-test-create-react-app/static/js/main.25ff4f6b.chunk.js"
+  },
+  {
+    "revision": "0b6849a860ee29f232c3",
+    "url": "/react-test-create-react-app/static/js/runtime-main.7173ead6.js"
+  }
+]);
